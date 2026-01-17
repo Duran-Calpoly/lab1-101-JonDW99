3ef4d858c3f939ee512c5c1b09a72d69ae42c931
JonDW99 <116831469+JonDW99@users.noreply.github.com>
2026-01-17 02:43:19 +0000

    Completed Lab 1

--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,20 @@
-pass
+def check_multiple(number):
+    if (number % 3 == 0 and number % 5 == 0):
+        return True
+    else:
+        return False
+def check_password(input_string):
+    if (input_string == "Python123"):
+        return ("access granted")
+    else:
+        return ("access denied")
+    
+def calculate_federal_tax(salary):
+    if (salary <= 11000):
+        return 0.1 * salary
+    elif (salary > 11000 and salary <= 44725):
+        return 0.12 * salary
+    elif (salary > 44725 and salary <= 95375):
+        return 0.22 * salary
+    else:
+        return 0.24 * salary
